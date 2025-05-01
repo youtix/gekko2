5117eb150fff46bc6e29d76727e63af68b96d4b8
youtix <>
2025-05-01 10:18:14 +0200

    feat(indicators): implement Stochastic RSI indicator

--- src/indicators/momentum/stochasticRSI/stochasticRSI.indicator.ts
+++ src/indicators/momentum/stochasticRSI/stochasticRSI.indicator.ts
@@ -1,10 +1,49 @@
-import { Indicator } from '../../indicator';
+import { Indicator } from '@indicators/indicator';
+import { Stochastic } from '@indicators/momentum';
+import { RSI } from '@indicators/oscillators';
+import { Candle } from '@models/types/candle.types';
+import { isNil } from 'lodash-es';
 
-export class StochasticRSI extends Indicator {
-  public onNewCandle(/** candle: Candle*/): void {
-    throw new Error('Method not implemented.');
+export class StochasticRSI extends Indicator<'StochasticRSI'> {
+  private rsi: RSI;
+  private stoch: Stochastic;
+
+  constructor(
+    {
+      period = 14,
+      fastKPeriod = 5,
+      fastDPeriod = 3,
+      slowMaType = 'sma',
+    }: IndicatorRegistry['StochasticRSI']['input'] = {
+      period: 14,
+      fastKPeriod: 5,
+      fastDPeriod: 3,
+      slowMaType: 'sma',
+    },
+  ) {
+    super('StochasticRSI', { fastK: null, fastD: null });
+    this.rsi = new RSI({ period });
+    this.stoch = new Stochastic({
+      fastKPeriod,
+      slowKPeriod: 1,
+      slowKMaType: slowMaType,
+      slowDPeriod: fastDPeriod,
+      slowDMaType: slowMaType,
+    });
   }
-  public getResult(): number | null {
-    throw new Error('Method not implemented.');
+
+  public onNewCandle(candle: Candle): void {
+    this.rsi.onNewCandle(candle);
+    const rsiValue = this.rsi.getResult();
+    if (isNil(rsiValue)) return;
+
+    this.stoch.onNewCandle({ high: rsiValue, low: rsiValue, close: rsiValue } as Candle);
+    const { k: fastK, d: fastD } = this.stoch.getResult();
+
+    if (!isNil(fastK) && !isNil(fastD)) this.result = { fastK, fastD };
+  }
+
+  public getResult() {
+    return this.result;
   }
 }
--- src/indicators/momentum/stochasticRSI/stochasticRSI.types.ts
+++ src/indicators/momentum/stochasticRSI/stochasticRSI.types.ts
@@ -1,6 +1,16 @@
+import { MovingAverageTypes } from '@indicators/indicator.types';
+
 declare global {
   interface IndicatorRegistry {
-    StochasticRSI: { input: null; output: null };
+    StochasticRSI: {
+      input?: {
+        period?: number;
+        fastKPeriod?: number;
+        fastDPeriod?: number;
+        slowMaType?: MovingAverageTypes;
+      };
+      output: { fastK: number | null; fastD: number | null };
+    };
   }
 }
 
