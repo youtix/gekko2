1c723d3de17fa70427c32330ba9794f8ac993f62
youtix <>
2025-07-02 14:52:18 +0200

    feat(indicators): add bollinger bands inside obv results

--- src/indicators/volume/obv/obv.indicator.ts
+++ src/indicators/volume/obv/obv.indicator.ts
@@ -1,3 +1,4 @@
+import { BollingerBands } from '@indicators/volatility/bollingerBands/bollingerBands.indicator';
 import { Candle } from '@models/types/candle.types';
 import { isNil } from 'lodash-es';
 import { Indicator } from '../../indicator';
@@ -5,10 +6,12 @@ import { Indicator } from '../../indicator';
 export class OBV extends Indicator<'OBV'> {
   private prevClose?: number;
   private obv: number;
+  private bb: BollingerBands;
 
-  constructor() {
-    super('OBV', null);
+  constructor({ period = 14, stdevUp = 2, stdevDown = 2, maType = 'sma' }: IndicatorRegistry['OBV']['input'] = {}) {
+    super('OBV', { obv: null, ma: null, upper: null, lower: null });
     this.obv = 0;
+    this.bb = new BollingerBands({ period, stdevDown, stdevUp, maType });
   }
 
   public onNewCandle(candle: Candle): void {
@@ -20,8 +23,16 @@ export class OBV extends Indicator<'OBV'> {
     if (candle.close > this.prevClose) this.obv += candle.volume;
     else if (candle.close < this.prevClose) this.obv -= candle.volume;
 
+    this.bb.onNewCandle({ close: this.obv } as Candle);
+    const { lower, middle, upper } = this.bb.getResult();
+
     this.prevClose = candle.close;
-    this.result = this.obv;
+    this.result = {
+      obv: this.obv,
+      ma: middle,
+      upper: upper,
+      lower: lower,
+    };
   }
 
   public getResult() {
