7aa914301ed7f9346281b91abca0c4c827a95e16
youtix <>
2025-09-06 12:56:54 +0200

    feat(strategies): implement volume delta strategy

---
+++ src/strategies/volumeDelta/volumeDelta.types.ts
@@ -0,0 +1,19 @@
+export interface VolumeDeltaStrategyParams {
+  src: 'quote' | 'base';
+  short: number;
+  long: number;
+  signal: number;
+  output: 'volumeDelta' | 'macd' | 'signal' | 'hist';
+  thresholds: {
+    up: number;
+    down: number;
+    persistence: number;
+  };
+}
+
+export type VolumeDeltaTrend = {
+  duration: number;
+  persisted: boolean;
+  direction: 'up' | 'down' | 'none';
+  adviced: boolean;
+};
