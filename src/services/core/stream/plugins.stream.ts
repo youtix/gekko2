aca9e78ee8c88aa8943c7e0dd006aeb813337d6d
youtix <>
2025-10-25 23:34:03 +0200

    feat(plugins): merge paper trader with trader plugin

--- src/services/core/stream/plugins.stream.ts
+++ src/services/core/stream/plugins.stream.ts
@@ -2,6 +2,9 @@ import { StopGekkoError } from '@errors/stopGekko.error';
 import { Candle } from '@models/candle.types';
 import { Nullable } from '@models/utility.types';
 import { Plugin } from '@plugins/plugin';
+import { DummyExchange } from '@services/exchange/exchange.types';
+import { isDummyExchange } from '@services/exchange/exchange.utils';
+import { inject } from '@services/injecter/injecter';
 import { info } from '@services/logger';
 import { after, find } from 'lodash-es';
 import { Writable } from 'node:stream';
@@ -9,14 +12,21 @@ import { Writable } from 'node:stream';
 export class PluginsStream extends Writable {
   plugins: Plugin[];
   private hasClosed = false;
+  private readonly dummyExchange?: DummyExchange;
+
   constructor(plugins: Plugin[]) {
     super({ objectMode: true });
     this.plugins = plugins;
+    const exchange = inject.exchange();
+    if (isDummyExchange(exchange)) this.dummyExchange = exchange;
   }
 
   public async _write(chunk: Candle, _: BufferEncoding, done: (error?: Nullable<Error>) => void) {
     const flushEvents = this.flushEvents().bind(this);
     try {
+      // Forward candle to dummy exchange (if set by user) before all plugins
+      this.dummyExchange?.addCandle(chunk);
+      // Forward candle to all plugins
       for (const plugin of this.plugins) await plugin.processInputStream(chunk, flushEvents);
       done();
     } catch (error) {
