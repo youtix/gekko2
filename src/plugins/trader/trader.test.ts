16974db49cb4d0cfea945c91a5ce93b5703abd41
youtix <youtix@users.noreply.github.com>
2025-11-09 18:49:49 +0100

    test(trader): expect order completion event

--- src/plugins/trader/trader.test.ts
+++ src/plugins/trader/trader.test.ts
@@ -505,7 +505,7 @@ describe('Trader', () => {
       trader.onStrategyCreateOrder(advice);
       const order = (trader as any).orders[0];
 
-      order.emit(ORDER_CANCELED_EVENT);
+      order.emit(ORDER_COMPLETED_EVENT);
       await Promise.resolve();
       await Promise.resolve();
 
@@ -535,7 +535,7 @@ describe('Trader', () => {
       const order = (trader as any).orders[0];
       (order.createSummary as Mock).mockRejectedValue(new Error('summary failed'));
 
-      order.emit(ORDER_CANCELED_EVENT);
+      order.emit(ORDER_COMPLETED_EVENT);
       await Promise.resolve();
       await Promise.resolve();
 
